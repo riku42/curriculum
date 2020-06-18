9d0e965ac3ef321bf8f4046e28a2062f91272ea0
riku42 <66983227+riku42@users.noreply.github.com>
2020-06-18 11:40:13 +0900

    課題提出テスト
    配列の問題回答

---
+++ 1-2/Task1_2.java
@@ -0,0 +1,36 @@
+/**
+ * Task1-2 : 課題内容
+ *
+ * 本課題では、配列の使い方を学んでいきましょう。
+ * 問①から問⑥まであります。
+ * 指定された値と変数名を守って記述してください。
+ *
+ * 問⑤,⑥については、コメントを入れてください。
+ *
+ */
+public class Task1_2 {
+    public static void main(String[] args) {
+        // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
+        int country[] = {JAPAN,AMERICA,KOREA};
+        // ② ①で作成した配列の要素数を出力してください。
+        System.out.println("配列countryの要素数…" + country.length);
+        /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+         *   1番目（先頭）の要素に 「りんご」 を代入
+         *   2番目の要素に 「もも」 を代入
+         *   3番目の要素に 「ぶどう」 を代入
+         */
+         String[] strArray = new String[3];
+         strArray[0] = "りんご";
+         strArray[1] = "もも";
+         strArray[2] = "ぶどう";
+        // ④ ③で作成した配列の2番目の要素を出力しなさい。
+        System.out.println("strArray[1]… " + strArray[1]);
+        /* ⑤下記の処理について、何をしているのかコメントを記入してください。
+         *  int型の５つの要素を持つ配列intArrayを初期化する。
+         */
+        int[] intArray = { 10, 20, 30, 40, 50 };
+        // ⑥下記の処理について、何をしているのかコメントを記入してください。
+        // 配列intArrayの２番目の要素と５番目の要素の中身を表示する。
+        System.out.println(intArray[1] + intArray[4]);
+    }
+}
